c792854811316f4ca6d741e9ea2fabaac86c54dc
Didiboycodes <61579857+Iam-Didi@users.noreply.github.com>
2022-09-01 09:39:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//document.getElementById("count-el").innerText = 5
+
+// let bonusPoints = 50 
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50 
+// console.log(bonusPoints)
+
+// bonusPoints = 100 
+// bonusPoints = bonusPoints - 75 
+// console.log(bonusPoints)
+
+// bonusPoints = 25 
+// bonusPoints = bonusPoints + 45 
+// console.log(bonusPoints)
+
+function increment() {
+    console.log("the button was clicked")
+}
