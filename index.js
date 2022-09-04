c843646b3c331495371292b317cc15035d9f52b7
Didiboycodes <61579857+Iam-Didi@users.noreply.github.com>
2022-09-03 18:13:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-//document.getElementById("count-el").innerText = 5
 
-// let bonusPoints = 50 
-// console.log(bonusPoints)
 
-// bonusPoints = bonusPoints + 50 
-// console.log(bonusPoints)
 
-// bonusPoints = 100 
-// bonusPoints = bonusPoints - 75 
-// console.log(bonusPoints)
 
-// bonusPoints = 25 
-// bonusPoints = bonusPoints + 45 
-// console.log(bonusPoints)
+ let countEl = document.getElementById("count-el")
+ console.log(countEl)
+ let count = 0
 
-function increment() {
-    console.log("the button was clicked")
-}
+ function increment() {
+     count = count + 1
+     countEl.innerText = count
+     console.log(count)
+ }
+
+ function save() {
+     console.log(count)
+} 
+
+save()
+
+let welcomeEl = document.getElementById("welcome-el")
+let Name = "Hi Divine 👋, "
+let greeting = "Welcome To The Train Station"
+
+
+welcomeEl.innerText = Name + greeting 
+
+welcomeEl.innerText = welcomeEl.innerText + "👋"
+
+
+
+
+
+
